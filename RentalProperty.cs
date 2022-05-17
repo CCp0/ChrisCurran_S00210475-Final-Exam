374a36e75f69c5ba36bc9673b829bd241319eaa2
Christopher Curran <S00210475@mail.itsligo.ie>
2022-05-17 10:14:48 +0100

    Q1e

--- RentalProperty.cs
+++ RentalProperty.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ChrisCurran_S00210475_Final_Exam
 {
-    class RentalProperty
+    public class RentalProperty
     {
+        public enum RentalType { Flat, House, Share }
         public int ID { get; set; }
-        public Enum RentalType { get; set; }
+        public string TypeOfRental { get; set; }
         public string Location { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
@@ -17,6 +19,11 @@ namespace ChrisCurran_S00210475_Final_Exam
         public void RentIncrease(decimal pcIncrease)
         {
             Price = Price + (Price * pcIncrease/100);
+        }
+        public class PropertyDetails : DbContext
+        {
+            public PropertyDetails() : base("MyPropertyDetails") { }
+            public DbSet<RentalProperty> Properties { get; set; }
         }
     }
 }
