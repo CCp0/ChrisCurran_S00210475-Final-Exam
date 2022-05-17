ecf7a33acf331927a882d032a98b2ef22a48dec6
Christopher Curran <S00210475@mail.itsligo.ie>
2022-05-17 09:48:37 +0100

    Q1C & Q1D

---
+++ RentalProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChrisCurran_S00210475_Final_Exam
+{
+    class RentalProperty
+    {
+        public int ID { get; set; }
+        public Enum RentalType { get; set; }
+        public string Location { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public void RentIncrease(decimal pcIncrease)
+        {
+            Price = Price + (Price * pcIncrease/100);
+        }
+    }
+}
